8409a4dfccd014305a2ad9d50a7ef17c489afbff
Stephen Tsen <stephentsen@stephens-mbp.mynetworksettings.com>
2022-10-05 18:43:15 -0400

    - better comment

--- examples/simple/src/pages/api/source-token.ts
+++ examples/simple/src/pages/api/source-token.ts
@@ -2,14 +2,15 @@ import { TokenGenerator } from '@source-health/source-js/server'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const generator = new TokenGenerator(
-  process.env.SOURCE_KEY_ID || '',
-  process.env.SOURCE_KEY_SECRET || '',
+  process.env.SOURCE_KEY_ID || 'key_YYc96gyopG0klWfUOhQU',
+  process.env.SOURCE_KEY_SECRET ||
+    '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',
 )
 
 export default function (_req: NextApiRequest, res: NextApiResponse) {
   res.json({
     token: generator.generate({
-      member: process.env.SOURCE_MEMBER_ID || '',
+      member: process.env.SOURCE_MEMBER_ID || 'mem_519zW7gVTy19LS9uDQAe',
       expiration: new Date(Date.now() + 24 * 60 * 60 * 1000),
       scopes: [],
     }),
