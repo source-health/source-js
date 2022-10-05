3ce100b5b7c3245083f7d3e54806f52c06e8836a
Stephen Tsen <stephentsen@stephens-mbp.mynetworksettings.com>
2022-10-05 18:44:10 -0400

    - revert keys

--- examples/simple/src/pages/api/source-token.ts
+++ examples/simple/src/pages/api/source-token.ts
@@ -2,15 +2,14 @@ import { TokenGenerator } from '@source-health/source-js/server'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const generator = new TokenGenerator(
-  process.env.SOURCE_KEY_ID || 'key_YYc96gyopG0klWfUOhQU',
-  process.env.SOURCE_KEY_SECRET ||
-    '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',
+  process.env.SOURCE_KEY_ID || '',
+  process.env.SOURCE_KEY_SECRET || '',
 )
 
 export default function (_req: NextApiRequest, res: NextApiResponse) {
   res.json({
     token: generator.generate({
-      member: process.env.SOURCE_MEMBER_ID || 'mem_519zW7gVTy19LS9uDQAe',
+      member: process.env.SOURCE_MEMBER_ID || '',
       expiration: new Date(Date.now() + 24 * 60 * 60 * 1000),
       scopes: [],
     }),
